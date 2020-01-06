97f8cfc41ecfeaec4237bd60a9a59c05aae58248
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2020-01-06 16:30:45 +0530

    clone mscheme:

--- src/ep_osm_mscheme/dig_ep_osm_mscheme.erl
+++ src/ep_osm_mscheme/dig_ep_osm_mscheme.erl
@@ -91,7 +91,10 @@ fetch(D, From, Size, Fs) ->
 			#dcell {val=#link {
 				text="Edit",
 				url=io_lib:format("/ep_osm_mscheme?mode=edit&id=~s", [itf:idval(Doc)])
-			}}
+			}},
+			#dcell {
+				val=ite:button(clone, "Clone", {clone, itf:idval(Doc)})
+			}
 		]
 	end, Docs),
 
@@ -102,7 +105,8 @@ fetch(D, From, Size, Fs) ->
 	Header = [
 		#dcell {type=header, val="Name"},
 		#dcell {type=header, val="State"},
-		#dcell {type=header, val="Edit"}
+		#dcell {type=header, val="Edit"},
+		#dcell {type=header, val="Clone"}
 	],
 
 
@@ -136,6 +140,21 @@ layout() ->
 %------------------------------------------------------------------------------
 % events
 %------------------------------------------------------------------------------
+
+event({confirmation_yes, {clone, Id}}) ->
+	handle_clone_confirmed(Id);
+
+event({clone, Id}) ->
+
+	{ok, Doc} = ep_osm_mscheme_api:get(Id),
+	itl:confirmation(
+		#panel {class="mycenter", body=[
+			#p {text="Are you sure you want to clone?"},
+			#p {text=io_lib:format("~s, ~s", [itf:val(Doc, name), itf:val(Doc, state)])}
+		]},
+		{clone, Id}
+	);
+
 event({itx, E}) ->
 	ite:event(E).
 
@@ -144,6 +163,41 @@ event({itx, E}) ->
 %------------------------------------------------------------------------------
 % handler
 %------------------------------------------------------------------------------
+
+%
+% handle - clone confirmed
+%
+handle_clone_confirmed(Id) ->
+
+	%
+	% init
+	%
+	{ok, Doc} = ep_osm_mscheme_api:get(Id),
+
+
+	%
+	% build fs
+	%
+	NewName = io_lib:format("~s (~s)", [
+		itf:val(Doc, name), helper:epochtimetostring(helper:epochtime())
+	]),
+	FsToSave = [
+		itf:build(?OSMMSC(name), NewName),
+		itf:build(?OSMMSC(state), "draft"),
+		itf:d2f(Doc, ?OSMMSC({list_of_widgets, list_of_widgets}))
+	],
+
+
+	%
+	% save
+	%
+	case ep_osm_mscheme_api:save(FsToSave) of
+		{ok, NewDoc} ->
+			Uri = io_lib:format("/ep_osm_mscheme?mode=edit&id=~s", [itf:idval(NewDoc)]),
+			helper:redirect(Uri);
+		_Error ->
+			helper_ui:flash(error, "failed!", 5)
+	end.
 
 
 
