55ab7f38880aaa66b7352add6dafc96bad9f3072
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2022-07-11 15:32:33 +0530

    student - add grievance mechanism

--- src/ep_osm_eval/ep_osm_eval_student.erl
+++ src/ep_osm_eval/ep_osm_eval_student.erl
@@ -52,7 +52,8 @@ layout(TestId, CandidateId) ->
 	%
 	?AKIT({layout, card_list_group, [
 		layout_student_info(TFs, Fs),
-		layout:g(4, anpcandidate:layout_evaluator_marking_0(TFs, Fs, anpevaluator)),
+		layout_student_grievance(TFs, Fs),
+		layout_marking_scheme(TFs, Fs),
 		layout_answerpaper(TFs, Fs)
 	]}).
 
@@ -63,9 +64,11 @@ layout(TestId, CandidateId) ->
 %------------------------------------------------------------------------------
 
 
+%..............................................................................
 %
 % layout - student info
 %
+%..............................................................................
 
 layout_student_info(_TFs, Fs) ->
 	Es = layout:get(?VIEW, fields:getfields(Fs, [
@@ -78,19 +81,99 @@ layout_student_info(_TFs, Fs) ->
 	].
 
 
+%..............................................................................
+%
+% layout - marking scheme
+%
+%..............................................................................
 
+layout_marking_scheme(TFs, Fs) ->
+	layout:g(4, anpcandidate:layout_evaluator_marking_0(
+		TFs, Fs, anpevaluator)).
+
+
+%..............................................................................
 %
 % layout answer paper
 %
+%..............................................................................
+
 layout_answerpaper(TFs, Fs) ->
 	itl:wire_script("ANP.disable_selection();"),
 	layout:grow(layout:g(12, anpcandidate:layout_answerpaper(TFs, Fs))).
 
 
+%..............................................................................
+%
+% layout - student grievance
+%
+%..............................................................................
+
+layout_student_grievance(TFs, Fs) ->
+	layout_student_grievance(TFs, Fs, itf:val(Fs, anp_redressal_state)).
+
+layout_student_grievance(_TFs, Fs, RedressalState) when
+	RedressalState == "new";
+	RedressalState == [] ->
+
+	%
+	% init
+	%
+	FGrievance = fields:build(
+		anp_redressal_grievance,
+		itf:val(Fs, anp_redressal_grievance)
+	),
+	FsEdit = [
+		FGrievance#field {validators=[required, length_limit_500]}
+	],
+
+
+	%
+	% layout
+	%
+	Event = ite:get(submit_redressal, "Submit Redressal"),
+	itl:get(?EDIT, FsEdit, Event, table);
+
+layout_student_grievance(_TFs, Fs, _) ->
+	FsView = itf:find(itf:toui(Fs), [
+		anp_redressal_state,
+		anp_redressal_grievance
+	]),
+	itl:get(?VIEW, FsView, noevent, table).
+
+
 %------------------------------------------------------------------------------
 % events
 %------------------------------------------------------------------------------
-		 
+
+event({confirmation_yes,submit_redressal}) ->
+
+	%
+	% init
+	%
+	OsmExamId = wf:q(anptestid),
+	CandidateId = wf:q(anpid),
+	ExamDb = anpcandidates:db(OsmExamId),
+	{ok, Doc} = anpcandidates:getdoc(ExamDb, CandidateId),
+
+	%
+	% update
+	%
+	FsToSave = [
+		fields:build(anp_redressal_state, "submitted")
+	] ++ itf:uivalue([
+		fields:get(anp_redressal_grievance)
+	]),
+	{ok, _} = ep_osm_candidate_api:update(OsmExamId, Doc, FsToSave),
+	helper:redirect(wf:uri());
+
+
+event(submit_redressal) ->
+	itl:confirmation(
+		"Are you sure you want to sumit? Grievance cannot be changed after submission",
+		submit_redressal
+	);
+
 event(Event) ->
 	?D(Event).
 
