3951e232e50502399de93819df3eb7ac6288aad2
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2022-07-28 15:09:30 +0530

    website - cap center admin login link

--- src/ep_osm_web_landing.erl
+++ src/ep_osm_web_landing.erl
@@ -27,6 +27,7 @@ layout_nav() ->
 			#link {class="btn btn-link", text="Admin Login", url="/login"},
 			#link {class="btn btn-link", text="Evaluator Login", url="/login"},
 			#link {class="btn btn-link", text="Controller Login", url="/itxlogin"},
+			#link {class="btn btn-link", text="CAP Centre Login", url="/itxlogin"},
 			#link {class="btn btn-link", text="DTP Login", url="/itxlogin"}
 		]}
     ].
