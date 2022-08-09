ab16063aac819735e9a7d7c9be24a1184754f4b6
Ravindra Chaturvedi <ravindra_chaturvedi@weshinetech.biz>
2022-08-09 17:14:45 +0530

    add function to get config key - min time for evaluation

--- src/ep_osm_config.erl
+++ src/ep_osm_config.erl
@@ -5,3 +5,15 @@
 
 is_qc_enabled() ->
 	itxconfigs_cache:get2(ep_osm_enable_qc, false).
+
+%------------------------------------------------------------------------------
+% evaluator cannot submit until this much time has elapsed. Unit seconds.
+% Default 0 seconds.
+%------------------------------------------------------------------------------
+get_min_time_for_evaluation() ->
+	itxconfigs_cache:get2(
+		ep_osm_min_evaluation_time_in_secs,
+		0
+	).
+
+
